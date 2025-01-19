670c44a7a8c485b7d3cd032fb79c0df83d8f210e
Umar Faruk Nasir <86917307+faruk369@users.noreply.github.com>
2025-01-19 23:04:25 +0300

    Initial Commit

---
+++ SimpleWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by Faryk on 19.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
